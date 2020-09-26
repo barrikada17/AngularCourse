9275b4b1dda1e5923c7ca766281e807f19927956
barrikada17 <aitor_bezoya@hotmail.com>
2020-09-26 10:45:02 +0200

    Add Angular Funfamentals

--- Hello-World/src/app/app.module.ts
+++ Hello-World/src/app/app.module.ts
@@ -1,16 +1,24 @@
+import { CoursesService } from './courses.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CoursesComponent } from './courses.components';
+import { CourseComponent } from './course/course.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CoursesComponent,
+    CourseComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [
+    CoursesService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
